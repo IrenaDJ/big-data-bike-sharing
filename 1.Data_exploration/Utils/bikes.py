57e05183ca3a1a4f3c82bad8a46b5c5a720eff2e
IrenaDJ <irenadj96@gmail.com>
2020-08-07 13:32:49 +0200

    phase 1

---
+++ 1.Data_exploration/Utils/bikes.py
@@ -0,0 +1,95 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import *
+from math import radians, cos, sin, asin, sqrt
+
+#Haversine distance
+def get_distance(longit_a, latit_a, longit_b, latit_b):
+    # Transform to radians
+    longit_a, latit_a, longit_b, latit_b = map(radians, [longit_a,  latit_a, longit_b, latit_b])
+    dist_longit = longit_b - longit_a
+    dist_latit = latit_b - latit_a
+    # Calculate area
+    area = sin(dist_latit/2)**2 + cos(latit_a) * cos(latit_b) * sin(dist_longit/2)**2
+    # Calculate the central angle
+    central_angle = 2 * asin(sqrt(area))
+    radius = 6371
+    # Calculate Distance
+    distance = central_angle * radius * 1000
+    return abs(round(distance))
+
+
+def process_bikes(bikes_raw):
+    """ Parses raw bikes data and selects only neccessary columns. """
+    bike_data = bikes_raw
+    bike_data = bike_data.withColumn('duration', bike_data['Duration'].cast(IntegerType()))
+    bike_data = bike_data.withColumn('start_date', F.to_timestamp(bike_data['Start date'], "yyyy-MM-dd HH:mm:ss"))
+    bike_data = bike_data.withColumn('end_date', F.to_timestamp(bike_data['End date'], "yyyy-MM-dd HH:mm:ss"))
+    bike_data = bike_data.withColumn('start_station_id', bike_data['Start station number'])
+    bike_data = bike_data.withColumn('start_station_name', bike_data['Start station'])
+    bike_data = bike_data.withColumn('end_station_id', bike_data['End station number'])
+    bike_data = bike_data.withColumn('end_station_name', bike_data['End station'])
+    bike_data = bike_data.withColumn('bike_number', bike_data['Bike number'])
+    bike_data = bike_data.withColumn('member_type', bike_data['Member type'])
+
+    bike_data = bike_data.select('duration', 'start_date', 'end_date', 'start_station_id', 'start_station_name', 'end_station_id', 'end_station_name', 'bike_number', 'member_type')
+
+    bike_data = bike_data.withColumn('start_station_id', F.when(bike_data['start_station_id'].like('00000'), '31127').otherwise(bike_data['start_station_id']))
+    bike_data = bike_data.withColumn('end_station_id', F.when(bike_data['end_station_id'].like('00000'), '31127').otherwise(bike_data['end_station_id']))
+    bike_data = bike_data.withColumn('start_station_id', F.when(bike_data['start_station_id'].isin('31008', '32031'), None).otherwise(bike_data['start_station_id']))
+    bike_data = bike_data.withColumn('end_station_id', F.when(bike_data['end_station_id'].isin('31008', '32031'), None).otherwise(bike_data['end_station_id']))
+    bike_data = bike_data.dropna(subset=['start_station_id', 'end_station_id'])
+    
+    return bike_data
+
+def process_stations(stations_raw, spark):
+    """ Parses raw stations data and selects only used stations and neccessary columns. """
+    stations = stations_raw.select('id', 'name', 'terminalName', 'lat', 'long', 'nbBikes', 'nbEmptyDocks')
+
+    stations = stations.withColumn('lat', stations.lat.cast(DoubleType()))
+    stations = stations.withColumn('long', stations.long.cast(DoubleType()))
+    stations = stations.withColumn('nbBikes', stations.nbBikes.cast(IntegerType()))
+    stations = stations.withColumn('nbEmptyDocks', stations.nbEmptyDocks.cast(IntegerType()))
+
+    station_columns = ['id', 'name', 'terminalName', 'lat', 'long', 'nbBikes', 'nbEmptyDocks']
+    new_station = spark.createDataFrame([(597, 'Mount Vernon Ave & E Del Ray Ave', '31086', 38.826213, -77.058640, 5, 10)], station_columns)
+    stations = stations.union(new_station)
+
+    return stations
+
+def filter_bikes(bikes, start_date, end_date):
+    dates = (start_date,  end_date)
+    date_from, date_to = [F.to_timestamp(F.lit(s), "MM/dd/yyyy") for s in dates]
+    bikes_filtered = bikes.where((bikes.start_date > date_from) & (bikes.start_date < date_to))
+    return bikes_filtered
+
+
+def bikes_summary(bikes, stations):
+    member_summary = bikes.groupBy('member_type').count().sort(F.desc("count"))
+    member_summary.show()
+    bikes_summary = bikes.groupBy('bike_number').count().sort(F.desc("count"))
+    bikes_summary.show()
+    duration_summary = bikes.select([F.min("duration"), F.max("duration"), F.mean("duration"), F.stddev("duration")])
+    duration_summary.show()
+
+    #print statistics for stations check-ins and check-outs
+    check_in_summary = bikes.groupBy('start_station_id').count().sort(F.desc("count"))
+    check_in_summary.show()
+    check_out_summary = bikes.groupBy('end_station_id').count().sort(F.desc("count"))
+    check_out_summary.show()
+
+    #print statistics for distance
+    udf_get_distance = F.udf(get_distance)
+    stations_partial = stations.select('terminalName', 'lat', 'long')
+
+    bikes_joined = bikes.join(stations_partial.withColumnRenamed('lat','lat_start').withColumnRenamed('long','long_start'), bikes.start_station_id == stations.terminalName)
+    bikes_joined = bikes_joined.drop('terminalName')
+    bikes_joined = bikes_joined.join(stations_partial.withColumnRenamed('lat','lat_end').withColumnRenamed('long','long_end'), bikes_joined.end_station_id == stations.terminalName)
+    bikes_joined = bikes_joined.drop('terminalName')
+
+    bikes = bikes_joined.withColumn("abs_distance", udf_get_distance(
+        bikes_joined.long_start, bikes_joined.lat_start,
+        bikes_joined.long_end, bikes_joined.lat_end))
+
+    distance_summary = bikes.select([F.min("abs_distance"), F.max("abs_distance"), F.mean("abs_distance"), F.stddev("abs_distance")])
+    distance_summary.show()
---
+++ 1.Data_exploration/Utils/weather.py
@@ -0,0 +1,58 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import *
+
+
+def process_weather(weather_raw):
+    """ Parses raw weather data and selects only neccessary columns. """
+
+    weather = weather_raw.dropna(subset=['Station_ID', 'Date_Time'])
+    weather = weather.dropna(subset=['air_temp_set_1', 'wind_speed_set_1'])
+
+    weather_short = weather.withColumn('Date_Time', weather.Date_Time.substr(0, 16))
+
+    weather_cast = weather_short.withColumn('date_time', F.to_timestamp(weather_short.Date_Time, "MM/dd/yyyy HH:mm"))
+    weather_cast = weather_cast.withColumn('air_temp', weather_cast.air_temp_set_1.cast(DoubleType()))
+    weather_cast = weather_cast.withColumn('wind_speed', weather_cast.wind_speed_set_1.cast(DoubleType()))
+    weather_cast = weather_cast.withColumn('visibility', weather_cast.visibility_set_1.cast(DoubleType()))
+    weather_cast = weather_cast.withColumn('weather_summary', weather_cast.weather_summary_set_1d)
+    weather_cast = weather_cast.withColumn('weather_condition', weather_cast.weather_condition_set_1d)
+
+    weather = weather_cast
+    weather = weather.select('date_time', 'air_temp', 'wind_speed', 'weather_summary', 'weather_condition', 'visibility')
+    #From Farenheit to Celsius
+    weather = weather.withColumn('air_temp', (weather.air_temp - 32) * 5.0/9.0)
+
+    weather = weather.withColumn('weather_class', F.when(weather.weather_summary.rlike('(clear|scattered|sunny|obscured)'), 'sunny').otherwise(weather.weather_summary))
+    weather = weather.withColumn('weather_class', F.when(weather.weather_summary.rlike('(broken|overcast|cloudy)'), 'cloudy').otherwise(weather.weather_class))
+    weather = weather.withColumn('weather_class', F.when(weather.weather_summary.rlike('(light (drizzle|rain))|rain|drizzle'), 'light rain').otherwise(weather.weather_class))
+    weather = weather.withColumn('weather_class', F.when(weather.weather_summary.rlike('(heavy (drizzle|rain))|heavy_rain'), 'heavy rain').otherwise(weather.weather_class))
+    weather = weather.withColumn('weather_class', F.when(weather.weather_class.rlike('snow'), 'snowy').otherwise(weather.weather_class))
+    weather = weather.withColumn('weather_class', F.when(weather.weather_class.rlike('ice'), 'icy').otherwise(weather.weather_class))
+    weather = weather.withColumn('weather_class', F.when(weather.weather_class.rlike('thunder|squalls'), 'stormy').otherwise(weather.weather_class))
+    weather = weather.withColumn('weather_class', F.when(weather.weather_class.rlike('haze|mist|fog'), 'foggy').otherwise(weather.weather_class))
+    weather = weather.withColumn('weather_class', F.when(weather.weather_summary.isNull(), 'sunny').otherwise(weather.weather_class))
+
+    return weather
+
+def filter_weather(weather, start_date, end_date):
+    """ Fiters weather data by start and end date. """
+
+    dates = (start_date,  end_date)
+    date_from, date_to = [F.to_timestamp(F.lit(s), "MM/dd/yyyy") for s in dates]
+    weather_filtered = weather.where((weather.date_time > date_from) & (weather.date_time < date_to))
+    return weather_filtered
+
+
+def weather_summary(weather):
+    """ Summary shows classes percentage in dataset and min, max, mean and std of the attributes. """
+
+    class_summary = weather.groupBy('weather_class').count()
+    class_summary.show()
+    temp_summary = weather.select([F.min("air_temp"), F.max("air_temp"), F.mean("air_temp"), F.stddev("air_temp")])
+    temp_summary.show()
+    wind_summary = weather.select([F.min("wind_speed"), F.max("wind_speed"), F.mean("wind_speed"), F.stddev("wind_speed")])
+    wind_summary.show()
+    visibility_summary = weather.select([F.min("visibility"), F.max("visibility"), F.mean("visibility"), F.stddev("visibility")])
+    visibility_summary.show()
+    
